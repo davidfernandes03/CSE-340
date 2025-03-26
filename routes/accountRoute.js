a973875f1a1152d6bc73d30e95742ca8946d1edc
David da Rocha Fernandes <davidrfernandes03@gmail.com>
2025-03-26 15:12:05 +0000

    Team activity

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = new express.Router();
 const accountController = require("../controllers/accountController");
 const utilities = require("../utilities/");
-const regValidate = require('../utilities/account-validation');
+const validation = require('../utilities/account-validation');
 
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
@@ -12,8 +12,16 @@ router.get("/register", utilities.handleErrors(accountController.buildRegister))
 // Process the registration data
 router.post(
     "/register",
-    regValidate.registrationRules(),
-    regValidate.checkRegData,
+    validation.registrationRules(),
+    validation.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+);
+
+// Process the login attempt
+router.post(
+    "/login",
+    validation.loginRules(),
+    validation.checkLogData,
     utilities.handleErrors(accountController.registerAccount)
 );
 
